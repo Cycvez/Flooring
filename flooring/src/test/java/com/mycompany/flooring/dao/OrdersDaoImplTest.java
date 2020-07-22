728a3b2cb6376aceb9dc875691dd3f2d93cc38d2
Carlos Chavez <cycvez@gmail.com>
2020-07-22 12:29:10 -0500

    export works now

--- flooring/src/test/java/com/mycompany/flooring/dao/OrdersDaoImplTest.java
+++ flooring/src/test/java/com/mycompany/flooring/dao/OrdersDaoImplTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
